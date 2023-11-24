f5504a1106d5ada758be9bdfa3e70442a11647bf
nadira1187 <nadira35-1187@s.diu.edu.bd>
2023-11-24 10:58:41 +0600

    added basic route setup

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,12 @@
+import NavBar from "../shared/navbar/NavBar";
+
+
+const Home = () => {
+    return (
+        <div>
+            <NavBar></NavBar>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/pages/shared/navbar/NavBar.jsx
@@ -0,0 +1,11 @@
+
+
+const NavBar = () => {
+    return (
+        <div>
+            <h2>navbar</h2>
+        </div>
+    );
+};
+
+export default NavBar;
